52346d6227e5430d31515670baabf7d4ae6137e9
JesperBaltzersen <jesperbaltzersen@gmial.com>
2014-02-13 16:58:06 +0100

    databar igen igen next

--- Databar1/mm.c
+++ Databar1/mm.c
@@ -27,11 +27,11 @@
 int align(int data_size);
 
 typedef struct S {
-	int* prev;
-	int* next;
+	struct S* prev;
+	struct S* next;
 	void* data;
 	int data_size;
-	int data_and_header_size;
+	int data_and_header_size_aligned;
 	int status; //1 for full, 0 for empty
 } list_elem;
 /**
@@ -49,38 +49,45 @@ void* embedded_malloc(size_t size) {
  * \todo { Implement malloc here. }
 */
 	int mem_start;
-	int mem_end;
 	int init_before;
 	int header_size;
-	list_elem* prev_elem, next_elem;
-	list_elem this_elem;
-	int* temp;
+	list_elem *prev_elem, *next_elem, *this_elem;
 
-	header_size = sizeof(list_elem);
+	this_elem = (list_elem*)lowest_available_physical_memory;
+	header_size = align(sizeof(list_elem));
+
+	init_before = *(int* )top_of_available_physical_memory;
 
 	if (init_before != 39693895)
 	{
-		test til giyt
-		this_elem -> prev = lowest_available_physical_memory;
-		this_elem -> next = top_of_available_physical_memory;
-		this_elem -> data = lowest_available_physical_memory + header_size;
-		this_elem -> data_and_header_size = align(header_size + size);
+
+		this_elem -> prev = (list_elem *)lowest_available_physical_memory;
+		this_elem -> next = (list_elem *)top_of_available_physical_memory;
+		this_elem -> data = (void *)(lowest_available_physical_memory + header_size);
+		this_elem -> data_and_header_size_aligned = header_size + align(size);
 		this_elem -> data_size = size;
 		this_elem -> status = 1;
 
-		temp = &top_of_available_physical_memory;
-		*temp = 39693895;
+		*(int *)top_of_available_physical_memory = 39693895;
+
 	}else
 	{
-		for (; this_elem -> next != top_of_available_physical_memory; )
-		{
-			this_elem -> prev = prev_elem;
-			this_elem -> next = prev_elem -> next;
-			this_elem -> data = this_elem + header_size;
-			this_elem -> data_size = size;
-			this_elem -> data_and_header_size = align(header_size + size);
-			this_elem -> status = 1;
+		int search_on = 0;
 
+		while (search_on == 0)
+		{
+			int calc_space, needed_space;
+			calc_space = (int)next_elem - ((int)this_elem + align(size) + header_size);
+			needed_space = align(header_size) + align(size);
+
+			if (calc_space >= needed_space ){
+				this_elem -> prev = prev_elem;
+				this_elem -> next = prev_elem -> next;
+				this_elem -> data = this_elem + header_size;
+				this_elem -> data_size = size;
+				this_elem -> data_and_header_size_aligned = align(header_size + size);
+				this_elem -> status = 1;
+			}
 			prev_elem -> next = this_elem;
 
 			next_elem -> prev = this_elem;
@@ -89,15 +96,7 @@ void* embedded_malloc(size_t size) {
 		}
 	}
 
-
-
-
-
-	memory[mem_start] = list_elem->prev;
-	memory[mem_start+1] = list_elem->next;
-	memory[mem_start+2] = list_elem->mem_first_byte;
-
-	return 0;
+	return this_elem -> data;
 }
 
 int align(int data_size)
